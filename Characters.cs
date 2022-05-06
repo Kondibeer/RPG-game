650e966e29e0c861517ccb0c4aa855f79295c81f
Kenneth Lendvald <kenneth@efio.dk>
2022-05-06 09:29:51 +0200

    Add project files.

---
+++ Characters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_game
+{
+    internal class Characters
+    {
+    }
+}
+/*
+ * Name
+ * Lvl
+ * Stats (Strength, Intelligence, Wisdom, Dexterity, Constitution, Charisma)
+ * Xp
+ */
